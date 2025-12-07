ebb19dab7fbe7d3819a24e6d5e41e40467733a08
Praveen Singh <57175335+praveensingh1999@users.noreply.github.com>
2025-12-07 21:34:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 //your code here
+let touristSpots = ["The Virupaksha Temple", "Victoria Memorial", "Tajmahal"];
+
+// Function to remove articles
+function stripArticle(name) {
+  return name.replace(/^(a |an |the )/i, "").trim();
+}
+
+// Sort the array without articles
+touristSpots.sort((a, b) => {
+  let nameA = stripArticle(a).toLowerCase();
+  let nameB = stripArticle(b).toLowerCase();
+  return nameA.localeCompare(nameB);
+});
+
+// Insert into the DOM
+const ul = document.getElementById("band");
+touristSpots.forEach((spot) => {
+  const li = document.createElement("li");
+  li.textContent = spot;
+  ul.appendChild(li);
+});
 
